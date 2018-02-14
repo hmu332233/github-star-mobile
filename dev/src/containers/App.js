657f59888dc3e54275840b3c4a600078f1b43e6a
minung.han <hmu332233@gmail.com>
2018-02-15 02:42:50 +0900

    [Add] ui - GithubCard Component

--- dev/src/containers/App.js
+++ dev/src/containers/App.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 import Hello from 'components/Hello';
 import ButtonGroup from 'components/ButtonGroup';
 import Header from 'components/Header';
+import GithubCard from 'components/GithubCard';
+
+var fakeData = {
+  title: 'reactstrap',
+  description: 'Simple React Bootstrap 4 component',
+  link: '#',
+  updated_at: '2018-02-14T16:48:18Z'
+}
 
 class App extends Component {
     state = {
@@ -18,13 +26,15 @@ class App extends Component {
     }
     
     render() {
-        return (
-          <div>
-          	<Header/>
-          	<Hello name={`React with Webpack2 (${this.state.number})`}/>
-          	<ButtonGroup/>
+      return (
+        <div>
+          <Header/>
+          <div id="cards">
+            <GithubCard starredItem={fakeData}/>
           </div>
-        );
+
+        </div>
+      );
     }
 }
 
